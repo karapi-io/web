a40ada03ae59687287c7c1ca70b96ae2fa071842
prashant9428 <pathakprashant841@gmail.com>
2025-12-29 01:40:02 +0530

    feat: added vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { HelmetProvider } from 'react-helmet-async';
+import { Analytics } from '@vercel/analytics/react';
+
 
 // 1. Create the context object outside the render
 const helmetContext = {};
@@ -12,6 +14,7 @@ createRoot(document.getElementById('root')!).render(
     {/* 2. Pass it to the provider */}
     <HelmetProvider context={helmetContext}>
       <App />
+      <Analytics />
     </HelmetProvider>
   </StrictMode>,
 )
